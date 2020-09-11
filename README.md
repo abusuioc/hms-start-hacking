6e91d760ce840503938396222b071aee75cb839a
abusuioc <abusuioc@users.noreply.github.com>
2020-09-11 16:01:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hms-start-hacking
+Template project to immediately start experimenting with kits from Huawei Mobile Services
---
+++ app/src/main/java/hms/starter/MainActivity.kt
@@ -0,0 +1,11 @@
+package hms.starter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
