a2e76ad10c0653bd0a8970c0b3b247b74cd5b73e
Adi <adrian.dan.busuioc@gmail.com>
2020-09-14 21:21:57 +0200

    Align package name with AGC package name: com.huawei.hackzurich

--- app/src/androidTest/java/com/huawei/hackzurich/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/huawei/hackzurich/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package hms.starter
+package com.huawei.hackzurich
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("hms.starter", appContext.packageName)
+        Assert.assertEquals("hms.starter", appContext.packageName)
     }
 }
